713eb4d5f184f502b7f89f62b7a20d708a518090
Dan Munk <danmunk@gmail.com>
2008-12-26 23:48:32 -0800

    Added first implementation

--- lib/fixed_fields.rb
+++ lib/fixed_fields.rb
@@ -1,24 +1,35 @@
 module FixedFields
-  
   def self.included(base)
     base.extend FieldMethods
   end
   
   module FieldMethods
-    def fixed_fields(attribute, options={})
+    def fixed_field(attribute, options={})
       unless included_modules.include? InstanceMethods
         extend ClassMethods
         include InstanceMethods
+        
+        class_eval { cattr_accessor :field_info }
+        self.field_info = {}
       end
+      
+      options[:width] = options[:end] - options[:begin] if options[:width].blank?
+      self.field_info[attribute] = options      
     end
   end
   
   module ClassMethods
-    
+    def read_fields(record)
+      instance = self.new
+      self.field_info.each do |key, options|
+        instance[key] = record[options[:begin], options[:width]]
+      end
+      
+      instance
+    end
   end
   
-  module InstanceMethods
-    
+  module InstanceMethods    
   end
 end
 
